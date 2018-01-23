76ec86a458dfa388f046223b57b4fce412240bd6
ZhouYC <zhouyucong0421@foxmail.com>
2018-01-24 01:37:29 +0800

    首次添加，将之前的写的少量代码使用git管理。目前为止完成了部分界面的框架。明天继续写界面！

---
+++ src/cc/zhouyc/view/Main.java
@@ -0,0 +1,45 @@
+package cc.zhouyc.view;
+
+/*
+ *  Main.java 为ZPlayer的主界面显示代码。
+ *  通过加载由 SceneBuilder 生成的 FXML文件进行构建。
+ *  
+ *  Author: ZhouYC
+ */
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			this.primaryStage = primaryStage;
+			FXMLLoader loader = new FXMLLoader(
+					Main.class.getResource("Main.fxml")
+					);
+			AnchorPane root = (AnchorPane)loader.load();
+			MainController controller = loader.getController();
+			Scene scene = new Scene(root);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/cc/zhouyc/view/MainController.java
@@ -0,0 +1,50 @@
+package cc.zhouyc.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+
+public class MainController implements Initializable{
+
+	@FXML
+	private Button buttonPlay;
+	@FXML
+	private Slider sliderTime;
+	@FXML
+	private VBox vBoxMusicList;
+	
+	private String playOrder = "顺序播放";
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// 采用JavaFX事件响应编程模型，为button设置点击效果
+		initAllButtonAction();
+		sliderTime.setValue(05);
+		System.out.println("Here I am in MainController "+Thread.currentThread().getName());
+		
+	}
+
+	public void initAllButtonAction() {
+		
+		buttonPlay.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("音乐开始播放！");
+				
+			}
+		});
+		
+		
+	}
+}
